4f3c7a03b7f4a3ff964f0318a87122323a4d8d9e
Sid Natarajan <sid.nat@gmail.com>
2023-01-08 20:45:19 -0500

    created fake user data

---
+++ db/seeds/01_players.sql
@@ -0,0 +1,6 @@
+INSERT INTO players (name, email, password, wins) VALUES ('Sid', 'sid@gmail.com', 'password', 212);
+INSERT INTO players (name, email, password, wins) VALUES ('Max', 'max@gmail.com', 'password', 1915);
+INSERT INTO players (name, email, password, wins) VALUES ('Will', 'will@gmail.com', 'password', 37);
+INSERT INTO players (name, email, password, wins) VALUES ('Manny', 'manny@gmail.com', 'password', 345);
+INSERT INTO players (name, email, password, wins) VALUES ('Samantha', 'samantha@gmail.com', 'password', 223);
+INSERT INTO players (name, email, password, wins) VALUES ('June', 'june@gmail.com', 'password', 2);
