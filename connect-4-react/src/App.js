6e352349286291fb960228bafacd4fb1555a7bc2
Sid Natarajan <sid.nat@gmail.com>
2023-01-08 19:31:22 -0500

    changed colour of navbar to blue, added 'signed in as: user' to navbar

--- connect-4-react/src/App.js
+++ connect-4-react/src/App.js
@@ -6,16 +6,19 @@ import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
   return (
-    <Navbar bg="dark" variant="dark">
-    <Container>
-      <Navbar.Brand href="#home">Connect 4</Navbar.Brand>
-      <Nav className="me-auto">
-        <Nav.Link href="#game">New game</Nav.Link>
-        <Nav.Link href="#user">User page</Nav.Link>
-        <Nav.Link href="#leaderboard">Leaderboard</Nav.Link>
-      </Nav>
-    </Container>
-  </Navbar>
+    <Navbar bg="primary" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Connect 4</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#game">New game</Nav.Link>
+          <Nav.Link href="#user">User page</Nav.Link>
+          <Nav.Link href="#leaderboard">Leaderboard</Nav.Link>
+        </Nav>
+        <Navbar.Text>
+          Signed in as: <a href="#logout">Connect User</a>
+        </Navbar.Text>
+      </Container>
+    </Navbar>
   );
 }
 
