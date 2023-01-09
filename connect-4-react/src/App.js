9079807fb01c7a4b2e1874c93a4b6a201bd508ca
Sid Natarajan <sid.nat@gmail.com>
2023-01-08 19:07:28 -0500

    created Navbar

--- connect-4-react/src/App.js
+++ connect-4-react/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar bg="dark" variant="dark">
+    <Container>
+      <Navbar.Brand href="#home">Connect 4</Navbar.Brand>
+      <Nav className="me-auto">
+        <Nav.Link href="#game">New game</Nav.Link>
+        <Nav.Link href="#user">User page</Nav.Link>
+        <Nav.Link href="#leaderboard">Leaderboard</Nav.Link>
+      </Nav>
+    </Container>
+  </Navbar>
   );
 }
 
