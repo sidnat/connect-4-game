76edf781c1103ac2174d6ade457e677753a616db
Sid Natarajan <sid.nat@gmail.com>
2023-01-10 15:31:32 -0500

    updated boardlogic helper

--- connect-4-react/src/helpers/boardLogic.js
+++ connect-4-react/src/helpers/boardLogic.js
@@ -1,8 +1,9 @@
-const board = (x, y) => {
-  const board = [];
-  let gameSizeX;
-  let gameSizeY;
+const board = [];
+// user passed in numbers
+let gameSizeX;
+let gameSizeY;
 
+const setBoardSize = (x, y) => {
   //minimum board size 5x5
   //x coords 0 to x left to right, see line 21
   if (x > 5) {
@@ -33,13 +34,32 @@ const board = (x, y) => {
     }
     board.push(temp);
   }
+}
 
-  const userIsSelectingColumn = 0; //should be user inputted variable
+setBoardSize(7, 5);
 
-  board[4][userIsSelectingColumn] = '1'; //player 1 moves
-  board[3][userIsSelectingColumn] = '2'; //player 2 moves
-  console.log(board);
+//     x0 x1 x2 x3 x4 x5 x6
+// y0 [[0, 0, 0, 0, 0, 0, 0],
+// y1  [0, 0, 0, 0, 0, 0, 0],
+// y2  [0, 0, 0, 0, 0, 0, 0],
+// y3  [0, 0, 0, 0, 0, 0, 0],
+// y4  [0, 0, 0, 0, 0, 0, 0]]
 
+//userIsSelectingColumn will be an onclick column number passed through
+board[4][userIsSelectingColumn] = '1'; //player 1 moves
+board[3][userIsSelectingColumn] = '2'; //player 2 moves
+console.log(board);
+
+//     x0  x1 x2 x3 x4 x5 x6
+// y0 [[0,  0, 0, 0, 0, 0, 0],
+// y1  [0,  0, 0, 0, 0, 0, 0],
+// y2  [0,  0, 0, 0, 0, 0, 0],
+// y3 ['2', 0, 0, 0, 0, 0, 0],
+// y4 ['1', 0, 0, 0, 0, 0, 0]]
+
+const userIsSelectingColumn = 0; //should be user inputted variable
+
+const boardIteration = () => {
   // iterating backwards from y-max to y-min, aka bottom y coord to top y coord.
   console.log("Begin iterating to find free spot:");
   for (let i = gameSizeY - 1; i >= 0; i--) {
@@ -48,23 +68,32 @@ const board = (x, y) => {
     if (board[i][userIsSelectingColumn] !== '1' && board[i][userIsSelectingColumn] !== '2') {
       console.log(`Yes! No board piece found, I can drop a piece at x = ${userIsSelectingColumn}, y = ${i}`);
 
-      // create function to drop new player piece in
-      // make the logic for the sql game board position update // use boardgame.io instead
-      // flip turn value in sql db  
+      // array@xy = playeridpiece
 
       break;
     } else {
       console.log(`No! Player "${board[i][userIsSelectingColumn]}" board piece exists at column x = ${userIsSelectingColumn}, row y = ${i}`);
     }
-
-    // write a function to check board for winner, diagonal, anti-diagonal, horizontal ,vertical
-
-    // if winner exists, update db record with winner
   }
-};
+}
+
+// Begin iterating to find free spot:
+// column x = 0, row y = 4: ( true ), can I put my piece here? 
+// No! Player "1" board piece exists at column x = 0, row y = 4
+// column x = 0, row y = 3: ( true ), can I put my piece here? 
+// No! Player "2" board piece exists at column x = 0, row y = 3
+// column x = 0, row y = 2: ( false ), can I put my piece here? 
+// Yes! No board piece found, I can drop a piece at x = 0, y = 2
+
+// make the logic for the sql game board position update
+// use boardgame.io instead
+// flip turn value in sql db  
+
+// write a function to check board for winner, diagonal, anti-diagonal, horizontal ,vertical
+
+// if winner exists, update db record with winner
 
 //test board
-board(7, 5);
 
 //console log outputs (without xy coords on board)
 
@@ -75,13 +104,6 @@ board(7, 5);
 // y3 ['2', 0, 0, 0, 0, 0, 0],
 // y4 ['1', 0, 0, 0, 0, 0, 0]]
 
-// Begin iterating to find free spot:
-// column x = 0, row y = 4: ( true ), can I put my piece here? 
-// No! Player "1" board piece exists at column x = 0, row y = 4
-// column x = 0, row y = 3: ( true ), can I put my piece here? 
-// No! Player "2" board piece exists at column x = 0, row y = 3
-// column x = 0, row y = 2: ( false ), can I put my piece here? 
-// Yes! No board piece found, I can drop a piece at x = 0, y = 2
 
 
 
