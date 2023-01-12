c06acd38415805f0d04c277bf5a7499079e9627b
Sid Natarajan <sid.nat@gmail.com>
2023-01-12 04:29:41 -0500

    visually rendered game on the game-page and styled appropriately

--- connect-4-react/src/components/game-page.js
+++ connect-4-react/src/components/game-page.js
@@ -1,70 +1,20 @@
-// import { Client } from 'boardgame.io/react';
-
-// export default function GamePage(props) {
-//   // const { } = props;
-
-//   return Client({ game: Connect4 } );
-// }
-
-// import React, { useState } from "react";
-// import Connect4 from '../gameLogic'
-
-// const GamePage = () => {
-//   const [board, setBoard] = useState(Array(6).fill(null).map(() => Array(7).fill(null)));
-//   const [currentPlayer, setCurrentPlayer] = useState("red");
-
-//   const makeMove = (column) => {
-//     for (let i = 5; i >= 0; i--) {
-//       if (!board[i][column]) {
-//         let newBoard = board.slice();
-//         newBoard[i][column] = currentPlayer;
-//         setBoard(newBoard);
-//         break;
-//       }
-//     }
-//     setCurrentPlayer(currentPlayer === "red" ? "yellow" : "red");
-//     checkForEnd();
-//   };
-
-//   return (
-//     <div>
-//       <GameBoard board={board} makeMove={makeMove} />
-//     </div>
-//   );
-// };
-
-// export default GamePage;
-
-
-// import React from 'react';
-// import { Client } from 'boardgame.io/react';
-// import Connect4 from './game';
-// import Board from './board';
-
-// const Connect4Client = Client({
-//   game: Connect4,
-//   board: Board,
-// });
-
-// const GamePage = () => (
-//   <div>
-//     <Connect4Client playerID="0" />
-//   </div>
-// );
-
-// export default GamePage;
-
-
+import React from "react";
 import { Client } from 'boardgame.io/react';
-import Connect4 from "../gameLogic";
-import GAME from "./game";
+import { Connect4Board } from './board';
+import { Connect4 } from './game';
 import './game-page.css';
 
-export default function GamePage() {
-  return (
+const App = Client({
+  game: Connect4,
+  board: Connect4Board
+});
 
-    <div className='row topspace'>
-      <GAME></GAME>
+export default function GamePage(props) {
+  // const { } = props;
+
+  return (
+    <div className="row topspace">
+      < App />
     </div>
   );
 }
