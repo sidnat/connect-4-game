e71b9866bc26774320d4f83800580b725e57a396
doublefriedtofu <hurdonghyuk@gmail.com>
2023-01-12 15:27:20 -0500

    Add Table head to leaderboard Table

--- connect-4-react/src/components/leaderboard-page.js
+++ connect-4-react/src/components/leaderboard-page.js
@@ -1,49 +1,62 @@
 import React from "react";
-import { MDBTable, MDBTableBody, MDBContainer,MDBRow,MDBCol,MDBBreadcrumb,MDBBreadcrumbItem } from "mdb-react-ui-kit";
-import './leaderboard-page.css'
-
+import {
+  MDBTable,
+  MDBTableBody,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBBreadcrumb,
+  MDBBreadcrumbItem,
+  MDBTableHead,
+  MDBTableCell
+} from "mdb-react-ui-kit";
+import "./leaderboard-page.css";
 
 export default function LeaderboardPage(props) {
   const { players } = props;
 
   return (
     <div className="leaderboard">
-    <section style={{ backgroundColor: "#eee" }}>
-      <MDBContainer className="py-5">
-        <MDBRow>
-          <MDBCol>
-            <MDBBreadcrumb className="bg-light rounded-3 p-3 mb-4">
-              <MDBBreadcrumbItem>
-                <a href="/">Home</a>
-              </MDBBreadcrumbItem>
-              <MDBBreadcrumbItem active>Leaderboard</MDBBreadcrumbItem>
-            </MDBBreadcrumb>
-          </MDBCol>
-        </MDBRow>
+      <section>
+        <MDBContainer className="py-5">
+          <MDBRow>
+            <MDBCol>
+              <MDBBreadcrumb className="bg-light rounded-3 p-3 mb-4">
+                <MDBBreadcrumbItem>
+                  <a href="/">Home</a>
+                </MDBBreadcrumbItem>
+                <MDBBreadcrumbItem active>Leaderboard</MDBBreadcrumbItem>
+              </MDBBreadcrumb>
+            </MDBCol>
+          </MDBRow>
         </MDBContainer>
 
-
-      <div className="leaderboard-container">
-      <h1 className='leaderboard-title'>Leaderboard</h1>
-        <MDBTable
-          striped
-          bordered
-          borderColor="primary"
-          style={{ width: "50%", margin: "auto", paddingTop: "100px" }}
-        >
-          
-          <MDBTableBody>
-            {players.map((player) => (
-              <tr key={player.id}>
-                <td>{player.name}</td>
-                <td>{player.wins} wins</td>
+        <div className="leaderboard-container">
+          <h1 className="leaderboard-title">Leaderboard</h1>
+          <MDBTable
+            striped
+            bordered
+            borderColor="black"
+            style={{ width: "85%", margin: "auto"}}
+          >
+            <MDBTableHead color="primary-color" textWhite>
+              <tr>
+                <td><strong>Name</strong></td>
+                <td><strong>Wins</strong></td>
               </tr>
-            ))}
-          </MDBTableBody>
-        </MDBTable>
-      </div>
-    </section>
-    </div>
+            </MDBTableHead>
 
+            <MDBTableBody >
+              {players.map((player) => (
+                <tr key={player.id}>
+                  <td>{player.name}</td>
+                  <td>{player.wins} wins</td>
+                </tr>
+              ))}
+            </MDBTableBody>
+          </MDBTable>
+        </div>
+      </section>
+    </div>
   );
 }
