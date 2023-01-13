bfd52efc585c014f1dac0eaa2a5e0fb13f7bb907
doublefriedtofu <hurdonghyuk@gmail.com>
2023-01-12 21:48:00 -0500

    Deleted unused code

--- connect-4-react/src/index.js
+++ connect-4-react/src/index.js
@@ -3,28 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import LeaderboardPage from './components/leaderboard-page';
-
-// import ReactDOM from "react-dom/client";
-// import {
-//   createBrowserRouter,
-//   RouterProvider,
-// } from "react-router-dom";
-
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <App />
-//   }
-// ])
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <RouterProvider router={router} />
-//   </React.StrictMode>
-// );
-
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
