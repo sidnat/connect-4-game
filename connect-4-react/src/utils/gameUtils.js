774074303d66b8f0dae5000367ba3db6c8525b7f
Sid Natarajan <sid.nat@gmail.com>
2023-01-18 09:45:55 -0500

    thoroughly commented custom win logic

--- connect-4-react/src/utils/gameUtils.js
+++ connect-4-react/src/utils/gameUtils.js
@@ -1,26 +1,10 @@
 export const setBoardSize = (x, y) => {
   const board = [];
-  let gameSizeX;
-  let gameSizeY;
-
-  //x coords 0 to x left to right, see line 31
-  if (x > 5 && x < 12) {
-    gameSizeX = x;
-  } else {
-    gameSizeX = 7;
-  }
-
-  //y coords 0 to x top to bottom , see line 31
-  if (y > 5 && x < 12) {
-    gameSizeY = y;
-  } else {
-    gameSizeY = 6;
-  }
 
   //custom board creator
-  for (let i = 0; i < gameSizeY; i++) {
+  for (let i = 0; i < y; i++) {
     const temp = [];
-    for (let j = 0; j < gameSizeX; j++) {
+    for (let j = 0; j < x; j++) {
       temp.push(null);
     }
     board.push(temp);
@@ -38,7 +22,7 @@ export const setBoardSize = (x, y) => {
 };
 
 export function findLowestCellByColumn(cells, column) {
-  // iterates user selected column, if cell is null, return index position
+  // iterates user selected column form bottom to top, if cell is null, return index position
   for (let i = cells.length - 1; i >= 0; i--) {
     if (cells[i][column] === null) {
       return i;
@@ -46,13 +30,18 @@ export function findLowestCellByColumn(cells, column) {
   }
 }
 
+// horizontal win check
 const horizontalCheck = (board, columns, rows, connectX) => {
-  // horizontal win check
+  // iterate from bottom row to top
   for (let i = rows - 1; i >= 0; i--) {
+    // iterate from left to right as long as connectX length is met
     for (let j = 0; j < columns - (connectX - 1); j++) {
+      // begining at bottom left of board
       if (board[i][j] !== null) {
         // check player ID in a horizontal line to determine if "connectX" exist in a row
         for (let k = 0; k < connectX - 1; k++) {
+          //checking if two consecutive pieces are equal
+
           if (board[i][j + k] !== board[i][j + (k + 1)]) {
             break;
           }
@@ -67,13 +56,17 @@ const horizontalCheck = (board, columns, rows, connectX) => {
   return false;
 };
 
+// vertical win check
 const verticalCheck = (board, columns, rows, connectX) => {
+  // iterate columns bottom to top
   for (let i = 0; i < columns; i++) {
+    // iterate rows left to right
     for (let j = rows - 1; j >= rows - (connectX - 1); j--) {
+      // beginning at bottom left of board
       if (board[j][i] !== null) {
-
         // check player ID in a vertical line to determine if "connectX" exist in a row
         for (let k = 0; k < connectX - 1; k++) {
+          //checking if two consecutive pieces are equal
           if (board[j - k][i] !== board[j - (k + 1)][i]) {
             break;
           }
@@ -88,13 +81,17 @@ const verticalCheck = (board, columns, rows, connectX) => {
   return false;
 };
 
+// diagonal win check (bottom left to top right)
 const diagonalCheck = (board, columns, rows, connectX) => {
-  // diagonal win check (bottom left to top right)
+  // iterate rows bottom to top as long as connectX height is met
   for (let i = connectX - 1; i < rows; i++) {
+    // iterate columns left to right as long as connectX width is met
     for (let j = 0; j < columns - (connectX - 1); j++) {
+      // beginning at bottom left of board
       if (board[i][j] !== null) {
         //check player ID in a diagonal bottom left to top right line to determine if 4 exist in a row
         for (let k = 0; k < connectX - 1; k++) {
+          //checking if two consecutive pieces are equal
           if (board[i - k][j + k] !== board[i - (k + 1)][j + (k + 1)]) {
             break;
           }
@@ -109,13 +106,17 @@ const diagonalCheck = (board, columns, rows, connectX) => {
   return false;
 };
 
+// anti diagonal win check (top left to bottom right)
 const antiDiagonalCheck = (board, columns, rows, connectX) => {
-  // anti diagonal win check (top left to bottom right)
+  // iterate rows top to bottom as long as connectX height is met
   for (let i = 0; i < rows - (connectX - 1); i++) {
+    // iterate columns top to bottom as long as connectX width is met
     for (let j = 0; j < columns - (connectX - 1); j++) {
+      // beginning at top left of board
       if (board[i][j] !== null) {
         //check player ID in a reverse diagonal top left to bottom right line to determine if 4 exist in a row
         for (let k = 0; k < connectX - 1; k++) {
+          //checking if two consecutive pieces are equal
           if (board[i + k][j + k] !== board[i + (k + 1)][j + (k + 1)]) {
             break;
           }
@@ -130,25 +131,15 @@ const antiDiagonalCheck = (board, columns, rows, connectX) => {
   return false;
 };
 
-export function checkWinner(board, columns, rows, winCondition) {
-  //add wincondition parameters so you cant have connect 9 on a 5x5 board
-  // do it based on game board size, if winCondition is 
-  //minimum board size 7x6
-
-  let connectX;
-
-  if (winCondition > 4 && winCondition < 9) {
-    connectX = winCondition;
-  } else {
-    connectX = 4;
-  }
+// iterates through entire board to see if ConnectX win conditions are met
+export function checkWinner(board, columns, rows, connectX) {
 
   if (horizontalCheck(board, columns, rows, connectX) || verticalCheck(board, columns, rows, connectX) || diagonalCheck(board, columns, rows, connectX) || antiDiagonalCheck(board, columns, rows, connectX)) {
     return true;
   }
 }
 
-// Return false if null value exists in board, returns true if all cells in board are occupied and no further moves can be made
+// returns true if all cells in board are occupied and no further moves can be made, return false if null value exists in board,
 export function IsDraw(cells) {
   for (let i = 0; i < cells.length; i++) {
     for (let j = 0; j < cells[i].length; j++) {
