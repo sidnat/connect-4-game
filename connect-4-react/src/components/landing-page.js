e9c70672e47e62a7bd521e6299b51cd080ebafea
doublefriedtofu <hurdonghyuk@gmail.com>
2023-01-18 11:18:47 -0500

    Working header implemented

--- connect-4-react/src/components/landing-page.js
+++ connect-4-react/src/components/landing-page.js
@@ -14,10 +14,10 @@ export default function LandingPage() {
       Let's Play Connect 
     </p>
     <ul class="content__container__list">
+      <li class="content__container__list__item">2</li>
       <li class="content__container__list__item">4</li>
-      <li class="content__container__list__item">6</li>
+      <li class="content__container__list__item">7</li>
       <li class="content__container__list__item">9</li>
-      <li class="content__container__list__item">12</li>
     </ul>
   </div>
 </div>     
