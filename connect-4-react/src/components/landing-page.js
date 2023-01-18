e86fe20b0a8b32e22a3e058b5fdd6e74a16737b7
doublefriedtofu <hurdonghyuk@gmail.com>
2023-01-18 10:48:31 -0500

    Add welcome message adjusted header position

--- connect-4-react/src/components/landing-page.js
+++ connect-4-react/src/components/landing-page.js
@@ -1,15 +1,34 @@
 import React from "react";
-import './landing-page.css';
+import "./landing-page.scss";
+
 
 export default function LandingPage() {
-
   return (
     //clean up nested divs
+    
     <div className="fullBody">
-      <div className="hero">
+
+<div class="content">
+  <div class="content__container">
+    <p class="content__container__text">
+      Let's Play Connect 
+    </p>
+    <ul class="content__container__list">
+      <li class="content__container__list__item">4</li>
+      <li class="content__container__list__item">6</li>
+      <li class="content__container__list__item">9</li>
+      <li class="content__container__list__item">12</li>
+    </ul>
+  </div>
+</div>     
+ <div className="hero">
         <div className="mainimage">
           <div className="parent">
-            <img className="image1" src="/images/connect4transparent.png" alt="Connect4" />
+            {/* <img
+              className="image1"
+              src="/images/connect4transparent.png"
+              alt="Connect4"
+            /> */}
           </div>
         </div>
       </div>
