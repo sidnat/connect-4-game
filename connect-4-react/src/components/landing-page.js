9a9fadd8950b70f8924299a6685525a7e828915c
doublefriedtofu <hurdonghyuk@gmail.com>
2023-01-18 11:35:03 -0500

    Fixed; Now scrollable in all pages

--- connect-4-react/src/components/landing-page.js
+++ connect-4-react/src/components/landing-page.js
@@ -14,8 +14,8 @@ export default function LandingPage() {
       Let's Play Connect 
     </p>
     <ul class="content__container__list">
-      <li class="content__container__list__item">2</li>
       <li class="content__container__list__item">4</li>
+      <li class="content__container__list__item">5</li>
       <li class="content__container__list__item">7</li>
       <li class="content__container__list__item">9</li>
     </ul>
