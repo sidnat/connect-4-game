18e4090b9a2d2997bc6be98bef28b84d1257466b
Sid Natarajan <sid.nat@gmail.com>
2023-01-18 10:08:16 -0500

    added winCondition limitation logic, renders page only if user logged in

--- connect-4-react/src/components/game-page.js
+++ connect-4-react/src/components/game-page.js
@@ -3,18 +3,40 @@ import { Client } from 'boardgame.io/react';
 import { Connect4Board } from './board';
 import { Connect4 } from './game';
 import './game-page.css';
-
-
-// console.log("this is game sizes:", gameSizeX, gameSizeY)
+import {
+  MDBCol,
+  MDBContainer,
+  MDBRow,
+  MDBCard,
+  MDBCardBody,
+  MDBBreadcrumb,
+  MDBBreadcrumbItem,
+} from "mdb-react-ui-kit";
 
 export default function GamePage(props) {
-  // const { gameSizeX, gameSizeY } = props;
-
+  const { playerInfo } = props;
   const gameSizeX = localStorage.getItem('gameSizeX');
   const gameSizeY = localStorage.getItem('gameSizeY');
+  let winCondition = localStorage.getItem('winCondition');
+
+  // Win condition can't be more than the board width or height, minus one. so you cant have connect 9 on a 5x5 board
+  if (winCondition > gameSizeX - 1 || winCondition > gameSizeY - 1) {
+    if (gameSizeX === gameSizeY) {
+      winCondition = gameSizeX - 1
+      localStorage.setItem('winCondition', winCondition)
+    }
+    if (gameSizeX < gameSizeY) {
+      winCondition = gameSizeX - 1;
+      localStorage.setItem('winCondition', winCondition)
+    }
+    if (gameSizeY < gameSizeX) {
+      winCondition = gameSizeY - 1;
+      localStorage.setItem('winCondition', winCondition);
+    }
+  }
 
   const Connect4Client = Client({
-    game: Connect4(gameSizeX, gameSizeY),
+    game: Connect4(gameSizeX, gameSizeY, winCondition),
     board: Connect4Board,
     debug: {
       collapseOnLoad: true,
@@ -22,9 +44,38 @@ export default function GamePage(props) {
     },
   });
 
-  return (
-    <div className="row topspace">
-      <Connect4Client />
-    </div>
-  );
+  // renders page based on logged in status
+  if (playerInfo.length !== 0) {
+    return (
+      <div className="row topspace">
+        <Connect4Client />
+      </div>
+    );
+  } else {
+    return (
+      <section style={{ backgroundColor: "#eee", height: "100vh" }}>
+        <MDBContainer className="py-5">
+          <MDBRow>
+            <MDBCol>
+              <MDBBreadcrumb className="bg-light rounded-3 p-3 mb-4">
+                <MDBBreadcrumbItem>
+                  <a href="/">Home</a>
+                </MDBBreadcrumbItem>
+                <MDBBreadcrumbItem active>Game</MDBBreadcrumbItem>
+              </MDBBreadcrumb>
+            </MDBCol>
+          </MDBRow>
+          <MDBRow>
+            <MDBCol>
+              <MDBCard className="mb-4">
+                <MDBCardBody className="text-center">
+                  <p className="text-muted mb-1"><strong>Please Login or Create User Profile</strong></p>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </section>
+    );
+  }
 }
